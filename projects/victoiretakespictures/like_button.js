4bee3438ec81bae8644a8f472fc6d58acca76c7e
Victoire Beaufils <victoire.beaufil@gmail.com>
2019-09-10 04:55:28 -0400

    Added coming soon page

---
+++ projects/victoiretakespictures/like_button.js
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return <Button variant="primary">You liked this.</Button>;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
